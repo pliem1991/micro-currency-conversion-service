dad1c99eb96f69120cb32d1181c3bc36af2f9794
Liem <pliem1991@gmail.com>
2019-03-18 17:02:00 +0700

    Call through API gateway

--- src/main/java/com/liempt/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ src/main/java/com/liempt/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -11,11 +11,11 @@ import com.liempt.microservices.currencyconversionservice.bean.CurrencyConversio
  * @author Liem.pt - 2019/03/18 13:15:00
  *
  */
-@FeignClient(name = "currency-exchange-service")
+@FeignClient(name = "netflix-zuul-api-gateway-server")
 @RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
-	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	@GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
 	public CurrencyConversionBean retrieveExchangeValue(@PathVariable("from") String from,
 			@PathVariable("to") String to);
 }
